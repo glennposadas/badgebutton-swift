1fab2346c53a8a1397db338f0529072a421c9562
Glenn Posadas <hello@glennvon.com>
2023-09-21 19:12:04 +0800

    add improvements. increase touch area to include the badgeViews. improve sample usage

--- BadgeButton/BadgeButton.swift
+++ BadgeButton/BadgeButton.swift
@@ -17,6 +17,10 @@ import UIKit
  let b = BadgeButton(icon: UIImage(named: "bell", shouldLimitValueTo9: true)!)
  ```
  
+ Warning:
+ If you're considering in stacking multiple instance of this control in a stackVIew, tapping the badge might not work.
+ Put them outside the stackView and this issue will not happen.
+ 
  */
 @objc
 class BadgeButton: UIButton {
@@ -32,13 +36,14 @@ class BadgeButton: UIButton {
   private(set) var shouldLimitValueTo9: Bool = false
   /// Reference to prevent blinking animation
   private(set) var currentBadgeCount: Int = 0
-  
+  /// Included in the touch area of this whole control.
   private lazy var badgeBGView: UIView = {
     let v = UIView()
     v.backgroundColor = defaultRedBadgeBG
     v.layer.cornerRadius = defaultCornerRadius
     v.translatesAutoresizingMaskIntoConstraints = false
     v.alpha = 0
+    v.isUserInteractionEnabled = false
     v.addSubview(badgeCountLabel)
     NSLayoutConstraint.activate([
       badgeCountLabel.topAnchor.constraint(equalTo: v.topAnchor),
@@ -56,6 +61,7 @@ class BadgeButton: UIButton {
     l.font = .systemFont(ofSize: defaultFontSize)
     l.layer.cornerRadius = defaultCornerRadius
     l.translatesAutoresizingMaskIntoConstraints = false
+    l.isUserInteractionEnabled = false
     return l
   }()
   
@@ -94,6 +100,16 @@ class BadgeButton: UIButton {
       badgeBGView.topAnchor.constraint(equalTo: topAnchor, constant: 4),
       badgeBGView.leadingAnchor.constraint(equalTo: trailingAnchor, constant: -8)
     ])
+  }
+  
+  override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+    let newArea = CGRect(
+      x: self.bounds.origin.x - 5.0,
+      y: self.bounds.origin.y - 5.0,
+      width: self.bounds.size.width + 30.0,
+      height: self.bounds.size.height + 10.0
+    )
+    return newArea.contains(point)
   }
   
   required init?(coder: NSCoder) {
--- BadgeButton/ViewController.swift
+++ BadgeButton/ViewController.swift
@@ -2,29 +2,56 @@ import UIKit
 
 class ViewController: UIViewController {
   
-  let bell1 = BadgeButton(icon: UIImage(named: "bell")!)
-  let bell2 = BadgeButton(icon: UIImage(named: "bell")!, shouldLimitValueTo9: true)
-  lazy var sv: UIStackView = {
-    let s = UIStackView(arrangedSubviews: [bell1, bell2])
-    s.axis = .horizontal
-    s.translatesAutoresizingMaskIntoConstraints = false
-    s.spacing = 50
-    return s
-  }()
-  
+  let bell1 = BadgeButton(icon: UIImage(systemName: "bell.fill")!)
+  let bell2 = BadgeButton(icon: UIImage(systemName: "bell.fill")!, shouldLimitValueTo9: true)
+
   var badgeCount = 0
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    view.addSubview(sv)
+    view.addSubview(bell1)
+    view.addSubview(bell2)
+    
+    setupConstraints()
+    addTargets()
+  }
+  
+  private func setupConstraints() {
+    bell1.translatesAutoresizingMaskIntoConstraints = false
+    bell2.translatesAutoresizingMaskIntoConstraints = false
+    
     NSLayoutConstraint.activate([
-      sv.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-      sv.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      sv.heightAnchor.constraint(equalToConstant: 44)
+      bell1.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+      bell1.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -50),
+      bell1.heightAnchor.constraint(equalToConstant: 44),
+      
+      bell2.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+      bell2.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 50),
+      bell2.heightAnchor.constraint(equalToConstant: 44),
     ])
   }
   
+  private func addTargets() {
+    bell1.addTarget(self, action: #selector(bell1Tapped), for: .touchUpInside)
+    bell2.addTarget(self, action: #selector(bell2Tapped), for: .touchUpInside)
+  }
+  
+  // MARK: -
+  // MARK: Bell Selectors
+  
+  @objc
+  private func bell1Tapped() {
+    print("bell1Tapped")
+  }
+  
+  @objc
+  private func bell2Tapped() {
+    print("bell2Tapped")
+  }
+  
+  // MARK: -
+  // MARK: Test Buttons
   
   @IBAction func increment(_ sender: Any) {
     badgeCount += 1
